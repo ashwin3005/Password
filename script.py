c9c857d7148fe871b55246243738c5a266c853c7
ashwin3005 <sashwin@protonmail.com>
2022-08-05 09:57:47 +0530

    password generator

--- script.py
+++ script.py
@@ -1,9 +1,34 @@
 import random
 
+# reading w.txt
 with open('w.txt') as f:
-    
     words = [line.rstrip() for line in f]
 
-# printing elements from list
-print(random.sample(words, 5))
 
+symbols = '!@#$%&<>?'
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+num = '1234567890'
+
+random_word = random.sample(words, 1)    #list
+
+
+a = random_word[0]
+b = random.choice(list(num))
+c = random.choice(list(alpha))
+d = random.choice(list(symbols))
+
+coin = random.randint(0,1)     # flipping a coin
+
+password = []
+
+
+if coin == 1:
+    password.extend([a.upper(),b,c,d])
+else:
+    password.extend([a,b,c.upper(),d])
+
+random.shuffle(password)
+
+password = ''.join([str(elem) for elem in password])
+  
+print(password) 
